1854fd6e1c036b1064ec5a865e1037e3493f536d
Zhenyin Yang <zyang@ebay.com>
2015-03-14 22:15:16 -0700

    Add implementation

--- src/com/leetcode/zyang/L0100_SameTree.java
+++ src/com/leetcode/zyang/L0100_SameTree.java
@@ -20,16 +20,35 @@ public class L0100_SameTree {
 		// TODO Auto-generated method stub
 		L0100_SameTree sameTree = new L0100_SameTree();
 
-		int m = 3;
-		int n = 3;
+		CreateBinarySearchTree t1 = new CreateBinarySearchTree();
+		t1.add(1,2,3,4,5,6,7,8,9);
 		
-		TreeNode t1 = Util.createTree(m);
-		TreeNode t1 = Util.createTree(n);
+		CreateBinarySearchTree t2 = new CreateBinarySearchTree();
+		t2.add(1,2,3,4,5,6,7,8);
+		
+		System.out.println( " t1 ->  " + t1.toString());
+		System.out.println( " t2 ->  " + t2.toString());
+		
+		boolean result = sameTree.isSameTree(t1.root, t2.root);
+		
+		System.out.println("result = " + result);
 	}
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
         boolean result = false;
         
+        
+        if (p != null && q!= null ) {
+        	if (p.val != q.val)
+            	return false;
+        	
+        	result = isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } 
+        else  if ((p != null && q == null) || (p == null && q!= null)) 
+        	result = false; 
+        else 
+        	result = true;
+              
         return result;
     }
 }
