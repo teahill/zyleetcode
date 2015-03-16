1e4bae2fd16980d02dce25aebe70691a267e0b99
Zhenyin Yang <zyang@ebay.com>
2015-03-15 18:13:28 -0700

    Add impl

--- src/com/leetcode/zyang/L0104_MaxDepthBinaryTree.java
+++ src/com/leetcode/zyang/L0104_MaxDepthBinaryTree.java
@@ -1,5 +1,8 @@
 package com.leetcode.zyang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * @author zyang
@@ -10,15 +13,7 @@ package com.leetcode.zyang;
  *  Tree Depth-first Search
  *  
  */
-/**
- * Definition for binary tree
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
 public class L0104_MaxDepthBinaryTree {
 
 	public L0104_MaxDepthBinaryTree() {
@@ -27,10 +22,59 @@ public class L0104_MaxDepthBinaryTree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		L0104_MaxDepthBinaryTree maxBTree = new L0104_MaxDepthBinaryTree();
+		
+		List<String> strs = new ArrayList<String>();		
+		strs.add("3");
+		strs.add("9");
+		strs.add("#");
+		strs.add("#");
+		strs.add("20");
+		strs.add("15");
+		strs.add("#");
+		strs.add("#");
+		strs.add("7");
+		strs.add("9");
+		strs.add("#");
+		strs.add("#");
+		strs.add("#");
+		
+		System.out.println("Input = " + strs);
+		
+		TreeNode root = BinaryTreeSerializer.deserialize(strs);
+		
+		System.out.println("result = " + BinaryTreeSerializer.serialize(root));
+		
+		int result = maxBTree.maxDepth(root);
+		
+		System.out.println("result = " + result);
 	}
 
+	//DFS
     public int maxDepth(TreeNode root) {
-        
+    	int depth = 0;
+    	
+    	if (root == null)
+    		return depth;
+    	   	
+    	depth = maxDepthRecursive(root, 0);
+    	
+    	return depth + 1;      
+    }
+    
+    public int maxDepthRecursive(TreeNode root, int depth) {  	
+    	if (root == null)
+    		return depth;
+    	
+    	int dLeft = depth;
+    	int dRight = depth;
+    	
+    	if (root.left != null)
+    		dLeft = maxDepthRecursive(root.left, depth + 1);
+    	
+    	if (root.right != null)
+    		dRight = maxDepthRecursive(root.right, depth + 1);
+	
+    	return (dLeft > dRight) ? dLeft : dRight;      
     }
 }
