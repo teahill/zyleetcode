ef62a3697d72138647d7fe59d67326c940110c54
Zhenyin Yang <zyang@ebay.com>
2015-11-19 16:56:47 -0800

    test branch

---
+++ src/com/leetcode/zyang/VersionControl.java
@@ -0,0 +1,15 @@
+package com.leetcode.zyang;
+
+public class VersionControl {
+
+	public VersionControl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isBadVersion(int version) {
+		if (version > 6)
+			return true;
+		else 
+			return false;
+	}
+}
